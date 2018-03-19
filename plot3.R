12c36db50c1706c7d3eee85e2308db1251636f8a
halleyzhu <36751608+halleyzhu@users.noreply.github.com>
2018-03-18 21:46:55 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,7 +13,10 @@ SM2 <- as.numeric(power$Sub_metering_2)
 SM3 <- as.numeric(power$Sub_metering_3)
 
 ## plot 3
+png("plot1.png", width=480, height=480)
+
 plot(datetime, SM1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, SM2, type="l", col="red")
 lines(datetime, SM3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
